00aeb5992db24fcfe94b4e7d234f6efe935ae107
Brett Hoes <bretthoes@gmail.com>
2024-04-27 19:58:54 -0400

    rename and update validator

---
+++ src/SharedCookbook.Api/Validators/CreateCookbookDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using SharedCookbook.Api.Data.Dtos;
+
+namespace SharedCookbook.Api.Validators;
+
+public class CreateCookbookDtoValidator : AbstractValidator<CreateCookbookDto>
+{
+    public CreateCookbookDtoValidator()
+    {
+        RuleFor(x => x)
+            .NotNull()
+            .WithMessage("Cookbook cannot be null.");
+
+        RuleFor(x => x.CreatorPersonId)
+            .GreaterThan(0)
+            .WithMessage("Invalid creator.");
+
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .WithMessage("Title is required.");
+
+        RuleFor(x => x.Title)
+            .Length(2, 18)
+            .WithMessage("Title must be 2-18 characters.");
+    }
+}
