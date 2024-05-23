d221f90cb4d0e9952f687b492613c5a26c62d0cc
Brett Hoes <bretthoes@gmail.com>
2024-05-22 23:40:46 -0400

    update seed with recipe details

--- src/SharedCookbook.Api/Services/SeedDataService.cs
+++ src/SharedCookbook.Api/Services/SeedDataService.cs
@@ -203,8 +203,144 @@ public class SeedDataService : ISeedDataService
                 Title = "Salmon Loaf",
                 Cookbook = cookbooks[0],
                 Author = people[1],
-                ImagePath = "assets/images/chicken_casserole.png",
+                ImagePath = "assets/images/baked-salmon-loaf.jpg",
                 BakingTimeInMinutes = 90
+            }
+        };
+
+        var nutritions = new List<RecipeNutrition>()
+        {
+            new()
+            {
+                RecipeNutritionId = 0,
+                Recipe = recipes[0],
+                Calories = 160,
+                Protein = 14,
+                Fat = 8,
+                Carbohydrates = 18,
+                Sodium = 60,
+            }
+        };
+
+        var ratings = new List<RecipeRating>() 
+        {
+            new()
+            { 
+                RecipeRatingId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                RatingValue = 5,
+                Created = DateTime.Now,
+                Person = people[1],
+                PersonId = people[1].PersonId
+            },
+            new()
+            {
+                RecipeRatingId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                RatingValue = 4,
+                Created = DateTime.Now,
+                Person = people[1],
+                PersonId = people[1].PersonId
+            }
+        };
+
+        var ingredients = new List<RecipeIngredient>()
+        {
+            new()
+            {
+                RecipeIngredientId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                IngredientName = "1 can salmon",
+                Ordinal = 1,
+                Optional = false,
+            },
+            new()
+            {
+                RecipeIngredientId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                IngredientName = "1/2 cup crushed saltine crackers",
+                Ordinal = 2,
+                Optional = false,
+            },
+            new()
+            {
+                RecipeIngredientId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                IngredientName = "1/2 cup milk",
+                Ordinal = 3,
+                Optional = false,
+            },
+            new()
+            {
+                RecipeIngredientId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                IngredientName = "1 large egg, beaten",
+                Ordinal = 4,
+                Optional = false,
+            },
+            new()
+            {
+                RecipeIngredientId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                IngredientName = "2 tablespoons melted butter",
+                Ordinal = 5,
+                Optional = false,
+            },
+            new()
+            {
+                RecipeIngredientId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                IngredientName = "salt and pepper to taste",
+                Ordinal = 6,
+                Optional = true,
+            }
+        };
+
+        var directions = new List<RecipeDirection>() 
+        {
+            new()
+            {
+                RecipeDirectionId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                Ordinal = 1,
+                ImagePath = null,
+                DirectionText = "Preheat the oven to 350 degrees F (175 degrees C).",
+            },
+            new()
+            {
+                RecipeDirectionId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                Ordinal = 2,
+                ImagePath = null,
+                DirectionText = "Combine salmon, crushed crackers, milk, egg, melted butter, salt, and pepper in a mixing bowl. Mix thoroughly.",
+            },
+            new()
+            {
+                RecipeDirectionId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                Ordinal = 3,
+                ImagePath = null,
+                DirectionText = "Press the salmon mixture into a lightly greased 9x5-inch loaf pan.",
+            },
+            new()
+            {
+                RecipeDirectionId = 0,
+                Recipe = recipes[0],
+                RecipeId = recipes[0].RecipeId,
+                Ordinal = 4,
+                ImagePath = null,
+                DirectionText = "Bake in the preheated oven for 45 minutes or until done.",
             }
         };
 
@@ -212,6 +348,10 @@ public class SeedDataService : ISeedDataService
         context.Cookbooks.AddRange(cookbooks);
         context.CookbookMembers.AddRange(members);
         context.Recipes.AddRange(recipes);
+        context.RecipeNutrition.AddRange(nutritions);
+        context.RecipeRatings.AddRange(ratings);
+        context.RecipeIngredients.AddRange(ingredients);
+        context.RecipeDirections.AddRange(directions);
     }
 }
 
