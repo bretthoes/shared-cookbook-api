e4d631bc35fb7fe16de684de5de0459580b723b6
Brett Hoes <bretthoes@gmail.com>
2024-06-11 22:42:43 -0400

    convert using statement

--- src/SharedCookbook.Api/Extensions/SeedDataExtension.cs
+++ src/SharedCookbook.Api/Extensions/SeedDataExtension.cs
@@ -7,14 +7,12 @@ public static class SeedDataExtension
 {
     public static void SeedData(this WebApplication app)
     {
-        using (var scope = app.Services.CreateScope())
-        {
-            var dbContext = scope.ServiceProvider.GetRequiredService<SharedCookbookContext>();
-            var seedDataService = scope.ServiceProvider.GetRequiredService<ISeedDataService>();
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<SharedCookbookContext>();
+        var seedDataService = scope.ServiceProvider.GetRequiredService<ISeedDataService>();
 
-            seedDataService.Initialize(dbContext);
-            dbContext.SaveChanges();
-        }
+        seedDataService.Initialize(dbContext);
+        dbContext.SaveChanges();
     }
 }
 
